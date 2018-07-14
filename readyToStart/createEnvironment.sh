48305a29c27cdfee993b7e9e373bbf9d8f8bde66
postaxmi <postaxmi@users.noreply.github.com>
2018-07-14 19:27:44 +0200

    create and load env for cluster

---
+++ readyToStart/createEnvironment.sh
@@ -0,0 +1,7 @@
+module load python
+virtualenv my_venv
+source my_venv/bin/activate
+pip install numpy
+pip install pandas
+pip install tensorflow-gpu
+pip install keras
---
+++ readyToStart/loadEnvironment.sh
@@ -0,0 +1,5 @@
+module load python
+source my_venv/bin/activate
+module load profile/deeplrn
+module load cuda
+module load cnn
